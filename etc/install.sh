ad86095c5743f0effd9d9b82c3c09ef95eb7ec9f
tadaren <ptymaato@yahoo.co.jp>
2019-08-25 22:43:30 +0900

    install.shのdownloadを更新

--- etc/install.sh
+++ etc/install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 DOTPATH=$HOME/dotfiles/
+DOTFILES_REPOSITORY="https://github.com/tadaren/dotfiles.git"
 
 exists(){
     type "$1" &> /dev/null
@@ -9,5 +10,18 @@ exists(){
 download(){
     if [ -d "$DOTPATH" ]; then
         echo "already exists"
+    fi
+
+    dotfiles_archive="https://github.com/tadaren/dotfiles/archive/master.tar.gz"
+    if exists "git"; then
+        git clone --recursive "$DOTFILES_REPOSITORY" "$DOTPATH"
+    elif exists "curl"; then
+        crul -L "$tar" | tar xvz
+    elif exists "wget"; then
+        wget -O - "$tar" | tar xvz
+    fi
+
+    if [ -d "dotfiles-master"]; then
+        mv "dotfiles-master" "$DOTPATH"
     fi
 }
